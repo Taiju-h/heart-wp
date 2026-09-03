ed9de9dfc8c8d1791056956e958e773fcd6bb6e0
heartfulschool-cmyk <heartfulschool@gmail.com>
2026-09-03 12:05:02 +0900

    Fix duplicate homepage news output

--- themes/test/htnew.php
+++ themes/test/htnew.php
@@ -1,8 +1,16 @@
 <ul>
-<?php query_posts('post_type=news&showposts=10');?>
-				<?php if(have_posts()): ?>
-				<?php while(have_posts()): the_post();?>
+<?php
+$heartful_news_query = new WP_Query(
+	array(
+		'post_type'      => 'news',
+		'posts_per_page' => 10,
+	)
+);
+?>
+				<?php if ($heartful_news_query->have_posts()): ?>
+				<?php while ($heartful_news_query->have_posts()): $heartful_news_query->the_post();?>
 <li><b><?php echo get_the_date( 'm-d' ); ?> </b><?php the_content(); ?></li>
 <?php endwhile; ?>
 <?php endif; ?>
+<?php wp_reset_postdata(); ?>
 </ul>
